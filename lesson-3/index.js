0add539b62854d136436feaf60c7933efb130e15
Sturrbon <sturrbon@outlook.com>
2019-04-03 11:47:29 +0800

    lesson-3 lesson-4

--- lesson-3/index.js
+++ lesson-3/index.js
@@ -12,12 +12,24 @@ app.set('port', process.env.PORT || 3000);
 // middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  '/css/bootstrap.css',
+  express.static('node_modules/bootstrap/dist/css/bootstrap.css')
+);
 
 // router
 app.get('/articles', (req, res, next) => {
   Article.all((err, articles) => {
     if (err) return next(err);
-    res.send(articles);
+    // res.send(articles);
+    res.format({
+      html: () => {
+        res.render('articles.ejs', { articles });
+      },
+      json: () => {
+        res.send(articles);
+      },
+    })
   })
 });
 
---
+++ lesson-4/webpack-example/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: './app/index.jsx',
+  output: { 
+    path: path.resolve(__dirname, 'dist'), 
+    filename: 'bundle.js', 
+    publicPath: '/assets/' 
+  },
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+};
